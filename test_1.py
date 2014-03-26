9edfa3c3ffb07332514d9f79948170af1c1c109b
albingrace <holmes.qian@gmail.com>
2014-03-26 01:32:37 -0500

    Create test_1.py

---
+++ test_1.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 22 16:07:31 2014
+
+@author: Qian Wan
+"""
+
+from time import time
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+txtfile = 'deals.txt'
+stop_list = set('for a of the if and to from as in online with at on learn item items month page you your thanks thank com shop'.split())
+n_features = 5000 # only consider top 5000 terms according to term frequency
+
+t0 = time() 
+    
+print('Getting corpus from documents...')
+print('top 10%% stop words are ignored and top %d terms are considered...') % n_features
+vectorizer = CountVectorizer(max_df=0.9, stop_words= stop_list, max_features=n_features)
+        
+Corpus = vectorizer.fit_transform(line for line in open(txtfile))
+print("done in %0.3fs." % (time() - t0))
+
+    
+freqs = [(word, Corpus.getcol(idx).sum()) for word, idx in vectorizer.vocabulary_.items()]
+sorted_freqs =  sorted(freqs, key = lambda x: -x[1])
+
+def Find_popular(): 
+    
+    # find the most popular term
+    most_popular = []
+    print('\nThe most popular terms are:')
+    for term,freq in sorted_freqs:
+        if freq==sorted_freqs[0][1]:
+            most_popular.append(term.encode("utf-8")) # decode from unicode to utf-8
+        else:
+            break
+        
+    print(most_popular)
+
+
+    #find the least popular term
+    least_popular = []
+    print('\nThe least popular terms are:')
+    for term,freq in reversed(sorted_freqs):
+        if freq==sorted_freqs[-1][1]:
+            least_popular.append(term.encode("utf-8")) # decode from unicode to utf-8
+        else:
+            break
+        
+    print(least_popular)
+    
+    
+
+#Find_popular()
+
+
+
+# find how many types of guitar are mentioned
+def Find_term(term_name):
+    # roughly find out the numbers of documents that contain Guitar
+    print('\nNumber of Guitar types by roughly computing: %d') % Corpus.getcol(vectorizer.vocabulary_.get(term_name)).sum()
+    
+    # when I looked into those documents, some of them did not mention any guitar type
+    # so I tried a different way: extract the word before "guitar" and eliminate some meaningless term
+    new_documents = [ line for line in open(txtfile) if term_name in line.lower().split()]
+    texts = [[word for word in document.lower().split() if word not in stop_list]
+             for document in new_documents]
+    
+    types = []
+    for line in texts:
+        types.append(line[line.index(term_name)-1])
+    
+    # find union of guitar types
+    types = set(types)
+    types = list(types)
+    
+    # delete terms that contain digit or punctuation letters
+    final_types = []
+    for term in types:
+        if term.isalpha():
+            final_types.append(term)
+    
+    print('\nNumber of Guitar types by filtering: %d') % len(final_types)
+    print('They are: %s') % final_types
+
